c0b192a05abdfa25a35a73cada9199ed8e2dd5fd
Pauli Tikka <30927365+paulitikka@users.noreply.github.com>
2025-02-20 11:31:14 +0200

    ok
    ok

--- extratsda.R
+++
@@ -1,4 +0,0 @@
-#"extrat sda"
-#.libPaths(c("/home/rstudio/renv/",.libPaths()))
-# .libPaths(c("C:/Program Files/R/R-4.4.1/library","C:/Users/patati/Documents/GitHub/Steroid_Data_Analysis/renv/library/R-4.3/x86_64-w64-mingw32"))
-# .libPaths(c("/home/rstudio/renv/library/R-4.3/x86_64-w64-mingw32","/home/rstudio/renv/library/windows/R-4.4/x86_64-w64-mingw32",.libPaths()))
--- okees.R
+++
@@ -1,3 +0,0 @@
-data <- c(1, 2, 3, 4, 5) 
-sum_data <- sum(data)
-print(sum_data) 
